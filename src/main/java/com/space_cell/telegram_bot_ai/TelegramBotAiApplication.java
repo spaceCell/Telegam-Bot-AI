6e3330dbe33cacfecd3fbaa4b0346a2aa9b85f27
Kirill Kozlov <space_cell@proton.me>
2024-06-03 14:42:03 +0500

    Init project

---
+++ src/main/java/com/space_cell/telegram_bot_ai/TelegramBotAiApplication.java
@@ -0,0 +1,13 @@
+package com.space_cell.telegram_bot_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space_cell/telegram_bot_ai/TelegramBotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space_cell.telegram_bot_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
